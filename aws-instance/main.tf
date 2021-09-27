2e711f9f90d392add2f8545dcf9020cf9a91f973
Stanislav Russu <stanislavrussu@macbookpro.local>
2021-09-27 22:41:34 +0300

    Introduction to IaC with TF

---
+++ aws-instance/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "remote" {
+    organization = "hashicorp-learn-stanislav-russu"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  # ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
